20de96c99a92d0989ee34f07a6f0fef82cb50240
GudinoJoaquin <JoaGudi@hotmail.com>
2025-07-14 10:12:12 -0300

    Conexion con railway y preparar para produccion

--- JS/JS7/config/connection.js
+++ JS/JS7/config/connection.js
@@ -1,10 +1,19 @@
 // Imporar el metodo createConnection de mysql2
 import { createConnection } from "mysql2";
 
-//Exportar el objeto de conexion como variable conn
+//Exportar el objeto de conexion como variable conn en localhost
+// export const conn = createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "score",
+// });
+
+//Exportar el objeto de conexion como variable conn en railway
 export const conn = createConnection({
-  host: "localhost",
+  host: "metro.proxy.rlwy.net",
+  port: 17269,
   user: "root",
-  password: "",
-  database: "score",
+  password: "KFlHZkqfRqtVtKtVMaffdAoiWhJDrfDm",
+  database: "railway",
 });
