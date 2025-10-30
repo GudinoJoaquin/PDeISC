a08cf2a3b43507d3c435703e8db51efa0ead4581
GudinoJoaquin <JoaGudi@hotmail.com>
2025-10-29 23:03:57 -0300

    Pagna de clases

--- Proyecto-final/api/profesor/controllers/getClass.js
+++ Proyecto-final/api/profesor/controllers/getClass.js
@@ -22,3 +22,30 @@ export async function getClass(req, res) {
     res.status(500).json({ error });
   }
 }
+
+export async function getClassByID(req, res) {
+  const { class_id } = req.params;
+
+  if (!class_id) return res.status(400).json({ error: "El id es requerido" });
+
+  try {
+    const { data, error } = await supabase
+      .from("clases")
+      .select()
+      .eq("id", class_id)
+      .single();
+
+    if (error) {
+      return res.status(500).json({ error });
+    }
+
+    if (!data) {
+      return res.status(404).json({ error: "No se encontro la clase" });
+    }
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+}
--- Proyecto-final/api/profesor/routes/profesor.js
+++ Proyecto-final/api/profesor/routes/profesor.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createClass } from "../controllers/createClass.js";
-import { getClass } from "../controllers/getClass.js";
+import { getClass, getClassByID } from "../controllers/getClass.js";
 
 const router = express.Router();
 
 router.get("/class/get/:profesor_id", getClass);
+router.get("/class/getByID/:class_id", getClassByID);
 router.post("/class/create", createClass);
 
 export default router;
